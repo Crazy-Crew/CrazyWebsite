4041e62b9a68269d7b5e0ad4923039ee561cf3ed
Ryder Belserion <no-reply@ryderbelserion.com>
2023-01-20 13:47:09 -0500

    Add favicon & page header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
+export default defineNuxtConfig({
+    app: {
+        buildAssetsDir: '/public',
+        head: {
+            title: 'CrazyCrew',
+            meta: [
+                {
+                    name: 'og:url',
+                    content: 'https://crazycrew.us'
+                }
+            ],
+            link: [
+                {
+                    rel: 'icon',
+                    type: 'image/x-icon',
+                    href: '/public/assets/favicon.ico'
+                }
+            ]
+        }
+    }
 })
